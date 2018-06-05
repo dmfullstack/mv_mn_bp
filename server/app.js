1abea62fb0e9e5980fabc085cbd70b6336770357
Dinesh Metkari <metkaridinesh@gmail.com>
2018-06-05 17:36:20 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const clientPath = path.resolve(__dirname, '..', 'dist');
+
+app.use(express.static(clientPath));
+
+app.get('/welcome', (req, res) => {
+  res.send({ msg: 'Welcome to app' });
+});
+
+app.use((req, res) => {
+  res.status(404).send({ error: 'Resource not found' });
+});
+
+module.exports = app;
